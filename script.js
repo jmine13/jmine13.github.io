d2393cf190bc672d41327fda05c02433bd7fed9a
jmine13 <jmine@me.com>
2026-07-08 04:59:03 +0900

    Separate HTML, CSS, and JS into external files

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+const lineSpacing = 5;
+const sampleStep = 10;
+const noiseScale1 = 0.007;
+const noiseScale2 = 0.02;
+const backgroundColor = [250, 248, 245];
+const strokeColor = [15, 25, 55, 60];
+
 let startMillis = 0;
 const stopAfterMs = 5000;
 let lineOffsets = [];
 let globalTimeOffset = 0;
 
 function initializeLineOffsets() {
-  const lineCount = Math.ceil(windowHeight / 5);
+  const lineCount = Math.ceil(windowHeight / lineSpacing);
   lineOffsets = [];
 
   for (let i = 0; i < lineCount; i++) {
@@ -32,9 +39,9 @@ function draw() {
   const progress = constrain(elapsed / stopAfterMs, 0, 1);
   const speed = max(0, 1 - progress * progress);
 
-  background(250, 248, 245);
+  background(...backgroundColor);
   noFill();
-  stroke(15, 25, 55, 60);
+  stroke(...strokeColor);
   strokeWeight(1);
 
   const elapsedSec = elapsed / 1000;
@@ -43,17 +50,17 @@ function draw() {
   const time = elapsedSec * freqFactor;
   const minAmpFactor = 0.25;
   const ampFactor = minAmpFactor + (1 - minAmpFactor) * speed;
-  const lineCount = Math.ceil(windowHeight / 5);
+  const lineCount = Math.ceil(windowHeight / lineSpacing);
 
   for (let i = 0; i < lineCount; i++) {
     beginShape();
     const offset = lineOffsets[i] || 0;
 
-    for (let x = 0; x < windowWidth; x += 10) {
-      const noiseValue1 = noise((i * 0.007) + time + offset + globalTimeOffset, x * 0.007);
-      const noiseValue2 = noise((i * 0.02) + (time * 1.8) + offset * 1.3 + globalTimeOffset * 0.01, x * 0.02);
+    for (let x = 0; x < windowWidth; x += sampleStep) {
+      const noiseValue1 = noise((i * noiseScale1) + time + offset + globalTimeOffset, x * noiseScale1);
+      const noiseValue2 = noise((i * noiseScale2) + (time * 1.8) + offset * 1.3 + globalTimeOffset * 0.01, x * noiseScale2);
       const combinedNoise = noiseValue1 * 0.7 + noiseValue2 * 0.3;
-      const y = (i * 5) + combinedNoise * windowHeight * 0.7 * ampFactor;
+      const y = (i * lineSpacing) + combinedNoise * windowHeight * 0.7 * ampFactor;
       vertex(x, y);
     }
 
